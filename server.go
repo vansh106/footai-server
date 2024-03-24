57cd6a7eb5febdaf4c00ea98ddcab1a1901c6ebd
vansh106 <vanshbulani6@gmail.com>
2024-03-24 23:51:32 +0530

    port change for railways

--- server.go
+++ server.go
@@ -78,8 +78,8 @@ func main() {
 
 	})
 
-	fmt.Println("Server listening on port 8080")
-	log.Fatal(router.Run(":8080"))
+	fmt.Println("Server listening on port 3000")
+	log.Fatal(router.Run(":3000"))
 
 }
 
